2e5ef5d7be71a85f5b3267c920fe0ed3dc00a837
Nimit Kumar <nimitkumar2002@gmail.com>
2023-03-01 22:06:47 +0530

    navbar iniatiated

---
+++ src/Views/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Components/Navbar_'
+
+const Homepage = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default Homepage
