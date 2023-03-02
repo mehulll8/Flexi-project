ad551890108c3cf9e0d90b99dda2f1f774d306da
Nimit Kumar <nimitkumar2002@gmail.com>
2023-03-02 19:13:23 +0530

    Homepage started Navbar almost complete'

--- src/Views/Homepage.jsx
+++ src/Views/Homepage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../Components/Navbar_'
+import '../App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Homepage = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <br></br><br></br>
+      <h1 className='m-auto'><b>Better clothing for the planet</b></h1>
+      <br></br>
+      <p className='m-auto p-auto'>Placeholder text here (will put our quote here)</p>
     </div>
   )
 }
