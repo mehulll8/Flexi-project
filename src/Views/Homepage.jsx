8b5ff6519cea10bc093f910b45fabe082218c7dc
Nimit Kumar <nimitkumar2002@gmail.com>
2023-03-02 23:42:55 +0530

    iniatilzation of carousel

--- src/Views/Homepage.jsx
+++ src/Views/Homepage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import Navbar from '../Components/Navbar_'
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Container } from 'react-bootstrap';
+import {Button,  Container } from 'react-bootstrap';
+import UncontrolledExample from '../Components/carousel';
+
+
 
 const Homepage = () => {
   return (
@@ -15,6 +18,9 @@ const Homepage = () => {
         <p className='m-auto p-auto'>Placeholder text here (will put our quote here)</p>
         <br></br>
         <Button variant="outline-primary" size="lg">Shop All</Button>
+        <br></br>
+        <br></br>
+        <UncontrolledExample></UncontrolledExample>
       </Container>
     </div>
   )
