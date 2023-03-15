67d931396eaa0a52fe27427a37499786a415b3ec
Nimit Kumar <nimitkumar2002@gmail.com>
2023-03-15 11:17:39 +0530

    just changed the size of placeholder image

--- src/Components/carousel.jsx
+++ src/Components/carousel.jsx
@@ -10,27 +10,27 @@ function UncontrolledExample() {
         <Button>
             <Carousel className='m-auto p-auto carousel'>
             <Carousel.Item>
-                <Image fluid src="https://placehold.jp/800x400.png"/>
+                <Image fluid src="https://placehold.jp/1200x400.png"/>
                 <Carousel.Caption>
                 <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                <p>First image</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <Image fluid src="https://placehold.jp/800x400.png"/>
+                <Image fluid src="https://placehold.jp/1200x400.png"/>
 
                 <Carousel.Caption>
                 <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Second Image</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <Image fluid src="https://placehold.jp/800x400.png"/>
+                <Image fluid src="https://placehold.jp/1200x400.png"/>
 
                 <Carousel.Caption>
                 <h3>Third slide label</h3>
                 <p>
-                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    This is the third image
                 </p>
                 </Carousel.Caption>
             </Carousel.Item>
