39915d65c435b222f5ed43c1ad00dbbbb86f286b
Nimit Kumar <nimitkumar2002@gmail.com>
2023-04-11 11:34:11 +0530

    Homepage done

--- src/Components/carousel.jsx
+++ src/Components/carousel.jsx
@@ -2,42 +2,26 @@ import { Button, Container } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
 import '../App.css';
 import '../bootstrap.min.css';
-import Image from 'react-bootstrap/Image'
+import ImgOverlay from '../Functions/Card';
 
 function UncontrolledExample() {
-  return (
-    <Container className='col-auto'>
-        <Button>
-            <Carousel className='m-auto p-auto carousel'>
-            <Carousel.Item>
-                <Image fluid src="https://placehold.jp/1200x400.png"/>
-                <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>First image</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <Image fluid src="https://placehold.jp/1200x400.png"/>
-
-                <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Second Image</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <Image fluid src="https://placehold.jp/1200x400.png"/>
-
-                <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                    This is the third image
-                </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            </Carousel>
-        </Button>
-    </Container>
-  );
+    return (
+        <Container className='col-auto'>
+            <Button>
+                <Carousel className='m-auto p-auto carousel'>
+                    <Carousel.Item>
+                        <ImgOverlay imgSrc="https://placehold.jp/1200x400.png" title="Image1" text="This is the first image"  />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <ImgOverlay imgSrc="https://placehold.jp/1200x400.png" title="Image2" text="This is the Second image" />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <ImgOverlay imgSrc="https://placehold.jp/1200x400.png" title="Image3" text="This is the Third image" />
+                    </Carousel.Item>
+                </Carousel>
+            </Button>
+        </Container>
+    );
 }
 
 export default UncontrolledExample;
--- src/Views/Homepage.jsx
+++ src/Views/Homepage.jsx
@@ -4,6 +4,7 @@ import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button,  Col,  Container, Image, Row } from 'react-bootstrap';
 import UncontrolledExample from '../Components/carousel';
+import ImgOverlay from '../Functions/Card';
 
 const Homepage = () => {
   return (
@@ -32,14 +33,15 @@ const Homepage = () => {
         <Row className='mb-4' style={{ scrollSnapAlign: 'start' }}>
           <Col className='text-center'>
             <h1 className='m-auto'><b>Our latest arrivals</b></h1>
-            <p className='m-auto'>Placeholdder text 2 (Contains description of discount, new clothing etc)</p>
+            <p className='m-auto'>Placeholder text 2 (Contains description of discount, new clothing etc)</p>
+            <Button variant='outline-primary' size='lg'>Shop All</Button>
           </Col>
         </Row>
         <Row className='justify-content-center mb-4' style={{ scrollSnapAlign: 'center' }}>
           <Col sm='auto' xs='auto' md='auto' lg='auto' className='d-flex justify-content-around'>
-            <Image  src="https://placehold.jp/400x800.png" altText="Image 1" className='mr-2 ml-2'/>
-            <Image  src="https://placehold.jp/400x800.png" altText="Image 2" className='mr-2 ml-2'/>
-            <Image  src="https://placehold.jp/400x800.png" altText="Image 3" className='mr-2 ml-2'/>
+            <ImgOverlay imgSrc="https://placehold.jp/400x600.png" title="Image1" text="This is the first image"  className='mr-2 ml-2'/>
+            <ImgOverlay imgSrc="https://placehold.jp/400x600.png" title="Image1" text="This is the first image"  className='mr-2 ml-2'/>
+            <ImgOverlay imgSrc="https://placehold.jp/400x600.png" title="Image1" text="This is the first image"  className='mr-2 ml-2'/>
           </Col>
         </Row>
       </Container>
